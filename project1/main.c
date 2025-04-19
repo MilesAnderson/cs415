7f28e4527fb3e911fe1e078a8bb57369dffdd959
Miles Anderson <milesanderson@Miless-MacBook-Pro.local>
2025-04-19 14:06:55 -0700

    main file is starting to look like a pseudoshell

--- project1/main.c
+++ project1/main.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
+    int whileparam = 0;
 
-    printf(">>> ");
+    while(whileparam == 0){
+        printf(">>> ");
 
-    read = getline(&line, &len, stdin);
-    if(read == -1){
-        perror(line);
+        read = getline(&line, &len, stdin);
+        if(read == -1){
+            perror(line);
+        }
+        line[strcspn(line, "\n")] = '\0';
+        if(strcmp(line, "exit") == 0){
+            whileparam++;
+        }
+
+        printf("%s\n", line);
     }
-
-    printf("%s\n", line);
 
     free(line);
 }
