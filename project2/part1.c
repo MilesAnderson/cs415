914d1d897cbfbd9c0af27b7577d62bfbaeee3f79
Miles Anderson <milesanderson@Miless-MacBook-Pro-4.local>
2025-05-15 15:08:21 -0700

    working part1

---
+++ project2/part1.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<string.h>
+#include<errno.h>
+
+int main(int argc, char *argv[]){
+    //Usage
+    if(argc != 2){
+        printf("Wrong number of arguments\n");
+        exit(0);
+    }
+    
+    //Opening file
+    FILE* input;
+    input = fopen(argv[1], "r");
+    if(input == NULL){
+        printf("The input file failed to open.\n");
+        return 1;
+    }
+
+    //For waiting
+    int n_children = 0;
+    pid_t child_pids[256];
+
+    //For each command
+    char line[1024];
+    while(fgets(line, sizeof(line), input) != NULL){
+
+        line[strcspn(line, "\n")] = '\0';
+
+        //Create the input for execvp
+        char* cmd_argv[64];
+        int i = 0;
+        char* token = strtok(line, " \t");
+        while(token != NULL && i < (int)(sizeof(cmd_argv)/sizeof(cmd_argv[0]))-1){
+            cmd_argv[i++] = token;
+            token = strtok(NULL, " \t");
+        }
+        cmd_argv[i] = NULL;
+
+        //Launch and run separate process
+        pid_t pid = fork();
+        if(pid < 0){
+            perror("fork");
+            continue;
+        }
+        else if(pid == 0){
+            execvp(cmd_argv[0], cmd_argv);
+            perror("execvp");
+            _exit(EXIT_FAILURE);
+        }
+        else{
+            child_pids[n_children++] = pid;
+        }
+    }
+
+    fclose(input);
+
+    //Wait for processes
+    for(int j = 0; j < n_children; j++){
+        int status;
+        if(waitpid(child_pids[j], &status, 0) < 0){
+            perror("waitpid");
+        }
+    }
+
+    _exit(EXIT_SUCCESS);
+}
