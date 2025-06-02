af8802bc65629bdd20b65c79ba933608ce8b5ad2
Miles Anderson <milesanderson@Miless-MacBook-Pro-4.local>
2025-06-01 21:54:17 -0700

    made some more progress on part1 and want to test with valgrind

--- project3/part1/park.c
+++ project3/part1/park.c
@@ -1,11 +1,5 @@
 #include "park.h"
 
-#define NUM_WORKERS 1
-
-pthread_t *attendants;
-
-static struct timespec start_time;
-
 static int elapsed_seconds(void){
     struct timespec now;
     clock_gettime(CLOCK_MONOTONIC, &now);
@@ -15,16 +9,108 @@ static int elapsed_seconds(void){
     return (int)sec_diff + (int)nsec_diff / 1e9;
 }
 
+void enterTicketLine(void){
+    unsigned long my_ticket = atomic_fetch_add(&next_ticket, 1UL);
+
+    while(atomic_load(&now_serving) != my_ticket){
+        struct timespec ts = {0, 100000};
+        nanosleep(&ts, NULL);
+    }
+}
+
+void exitTicketLine(void){
+    atomic_fetch_add(&now_serving, 1UL);
+}
+
 void* attendant(void* arg){
     int *id = (int *)arg;
-    printf("[Time: %.3d s] Thread %d has entered the park.\n", elapsed_seconds(), *id);
-    printf("[Time: %.3d s] Thread %d is exploring the park.\n", elapsed_seconds(), *id);
-    int min = 2,max = 5;
-    int range = max - min +1;
-    int r2to5 = (rand() % range) + min;
-    sleep(r2to5);
-    printf("[Time: %.3d s] Thread %d finished exploring, heading to ticket booth.\n", elapsed_seconds(), *id);
-    pthread_exit(NULL);
+
+    while(1){
+        printf("[Time: %.3d s] Thread %d has entered the park.\n", elapsed_seconds(), *id);
+        printf("[Time: %.3d s] Thread %d is exploring the park.\n", elapsed_seconds(), *id);
+        int min = 2,max = 5;
+        int range = max - min +1;
+        int r2to5 = (rand() % range) + min;
+        sleep(r2to5);
+
+        printf("[Time: %.3d s] Thread %d finished exploring, heading to ticket booth.\n", elapsed_seconds(), *id);
+        enterTicketLine();
+        printf("[Time: %.3d s] Thread %d waiting in ticket line.\n", elapsed_seconds(), *id);
+        exitTicketLine();
+        printf("[Time: %.3d s] Thread %d acquired a ticket.\n", elapsed_seconds(), *id);
+
+        sem_wait(&sem_boardQueue);
+        pthread_mutex_lock(&ride_mutex);
+            boardedCount++;
+            printf("[Time: %.3d s] Thread %d boarded (seat %d/%d).\n", elapsed_seconds(), *id, boardedCount, p);
+            if(boardedCount == p){
+                pthread_mutex_unlock(&ride_mutex);
+                sem_post(&sem_boardQueue);
+            }
+            else{
+                pthread_mutex_unlock(&ride_mutex);
+            }
+
+        sem_wait(&sem_unboardQueue);
+        pthread_mutex_lock(&ride_mutex);
+            unboardedCount++;
+            printf("[Time: %.3d s] Thread %d unboarded (count %d/%d).\n", elapsed_seconds(), *id, unboardedCount, p);
+            if(unboardedCount == p){
+                pthread_mutex_unlock(&ride_mutex);
+            sem_post(&sem_unboardQueue);
+            }
+            else{
+                pthread_mutex_unlock(&ride_mutex);
+            }
+    }
+
+    return NULL;
+}
+
+void *car(void *arg){
+    int round = 0;
+
+    while(1){
+        round++;
+        pthread_mutex_lock(&ride_mutex);
+        boardedCount = 0;
+        unboardedCount = 0;
+        pthread_mutex_unlock(&ride_mutex);
+        printf("[Time: %.3d s] Car load phase started. Waiting for up to %d passengers...\n", elapsed_seconds(), p);
+        for(int i = 0; i < p; i++){
+            sem_post(&sem_boardQueue);
+        }
+    
+
+        while(1){
+            pthread_mutex_lock(&ride_mutex);
+            int bc = boardedCount;
+            pthread_mutex_unlock(&ride_mutex);
+            if(bc == p){
+                break;
+            }
+            struct timespec ts = {0, 100000};
+            nanosleep(&ts, NULL);
+        }
+        printf("[Time: %.3d s] All %d passengers boarded. Departing (round %d).\n", elapsed_seconds(), p, round);
+        sleep(r);
+        printf("[Time: %.3d s] Ride has ended, car is unloading.\n", elapsed_seconds());
+        for(int i = 0; i < p; i++){
+            sem_post(&sem_unboardQueue);
+        }
+
+        while(1){
+            pthread_mutex_lock(&ride_mutex);
+            int uc = unboardedCount;
+            pthread_mutex_unlock(&ride_mutex);
+            if(uc == p){
+                break;
+            }
+            struct timespec ts = {0, 100000};
+            nanosleep(&ts, NULL);
+        }
+        printf("[Time: %.3d s] All %d passengers have unboarded. Ride complete (round %d).\n", elapsed_seconds(), p, round);
+    }
 }
 
 int main(int argc, char* argv[]){
@@ -34,11 +120,6 @@ int main(int argc, char* argv[]){
     }
 
     int opt;
-    int n = NUM_WORKERS;
-    int c = 1;
-    int p = 1;
-    int w = 3;
-    int r = 6;
 
     while((opt = getopt(argc, argv, ":n:c:p:w:r:h")) != -1){
         switch(opt){
@@ -88,6 +169,12 @@ int main(int argc, char* argv[]){
     printf("- Ride duration: %d seconds\n", r);
     printf("\n");
 
+    n = 1;
+    c = 1;
+
+    sem_init(&sem_boardQueue, 0, 0);
+    sem_init(&sem_unboardQueue, 0, 0);
+
     attendants = (pthread_t *)malloc(sizeof(pthread_t) * n);
 
     int *numbers = (int*)malloc(sizeof(int) * n);
@@ -95,6 +182,8 @@ int main(int argc, char* argv[]){
         numbers[i] = i+1;
     }
 
+    cars = malloc(sizeof(pthread_t) * c);
+    pthread_create(&cars[0], NULL, car, NULL);
 
     for(int i = 0; i < n; ++i){
         pthread_create(&attendants[i], NULL, attendant, (void*)&(numbers[i]));
@@ -104,8 +193,14 @@ int main(int argc, char* argv[]){
         pthread_join(attendants[j], NULL);
     }
 
+    pthread_join(cars[0], NULL);
+
     free(attendants);
     free(numbers);
+    free(cars);
+    sem_destroy(&sem_boardQueue);
+    sem_destroy(&sem_unboardQueue);
+    return 0;
 }
 
 
--- project3/part1/park.h
+++ project3/part1/park.h
@@ -1,6 +1,7 @@
 #ifndef PARK_H
 #define PARK_H
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,11 +10,42 @@
 #include <fcntl.h>
 #include <pthread.h>
 #include <time.h>
+#include <stdatomic.h>
+#include <semaphore.h>
+
+
+#define NUM_WORKERS 1
+
+pthread_t *attendants;
+pthread_t *cars;
+
+static struct timespec start_time;
+
+static atomic_ulong next_ticket = 0;
+static atomic_ulong now_serving = 0;
+
+int n = NUM_WORKERS;
+int c = 1;
+int p = 1;
+int w = 3;
+int r = 6;
+
+static sem_t sem_boardQueue;
+static sem_t sem_unboardQueue;
+static pthread_mutex_t ride_mutex = PTHREAD_MUTEX_INITIALIZER;
+static int boardedCount = 0;
+static int unboardedCount = 0;
+
 
 static int elapsed_seconds(void);
 
-void* ticketBooth(void);
+void enterTicketLine(void);
+
+void exitTicketLine(void);
 
 void* attendant(void* arg);
+
+void* car(void* arg);
+
 
 #endif
